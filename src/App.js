70e4ad22dee90d05b7a67e7fc1d336e32c752051
Sam Clearman <sam@scale.com>
2021-12-05 13:04:50 -0800

    skeleton

--- src/App.js
+++ src/App.js
@@ -6,51 +6,35 @@ import WPOD from "./artifacts/contracts/WPOD.sol/WPOD.json";
 const tokenAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
 function App() {
-  const [userAccount, setUserAccount] = useState();
-  const [amount, setAmount] = useState();
+  const [plotId, setPlotId] = useState();
 
   async function requestAccount() {
     await window.ethereum.request({ method: "eth_requestAccounts" });
   }
 
-  async function getBalance() {
-    if (typeof window.ethereum !== "undefined") {
-      const [account] = await window.ethereum.request({
-        method: "eth_requestAccounts",
-      });
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const contract = new ethers.Contract(tokenAddress, WPOD.abi, provider);
-      const balance = await contract.balanceOf(account);
-      console.log("Balance: ", balance.toString());
-    }
-  }
-
-  async function sendCoins() {
+  async function wrapPlot() {
     if (typeof window.ethereum !== "undefined") {
       await requestAccount();
       const provider = new ethers.providers.Web3Provider(window.ethereum);
       const signer = provider.getSigner();
       const contract = new ethers.Contract(tokenAddress, WPOD.abi, signer);
-      const transation = await contract.transfer(userAccount, amount);
+      console.log(plotId);
+      console.log(`wrapping plot ${plotId}`);
+      const transation = await contract.wrap(plotId, 0, 100);
       await transation.wait();
-      console.log(`${amount} Coins successfully sent to ${userAccount}`);
+      console.log(`Plot successfully wrapped!`);
     }
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <br />
-        <button onClick={getBalance}>Get Balance</button>
-        <button onClick={sendCoins}>Send Coins</button>
+        You own plots: 100,101,103
         <input
-          onChange={(e) => setUserAccount(e.target.value)}
-          placeholder="Account ID"
+          onChange={(e) => setPlotId(e.target.value)}
+          placeholder="Plot ID"
         />
-        <input
-          onChange={(e) => setAmount(e.target.value)}
-          placeholder="Amount"
-        />
+        <button onClick={wrapPlot}>Wrap Plot</button>
       </header>
     </div>
   );
